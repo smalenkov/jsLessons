30f40ea18925bb13642c4b9830c0324d7c8cd7df
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-09 13:42:48 +0300

    add tasks

--- 1_BasicJs/1-7_Others/2_json/docs.js
+++ 1_BasicJs/1-7_Others/2_json/docs.js
@@ -11,5 +11,110 @@
     alert(user.name); // 1
   }
 
-  window.run = runDoc_1;
+  // Пример 2
+
+  function runDoc_2() {
+    // дата в строке - в формате UTC
+    var str = '{"title":"Конференция","date":"2014-11-30T12:00:00.000Z"}';
+
+    var event = JSON.parse(str, function(key, value) {
+      if (key == 'date') return new Date(value);
+      return value;
+    });
+
+    alert( event.date.getFullYear() ); // теперь сработает!
+  }
+
+  // Пример 3
+
+  function runDoc_3() {
+    var room = {
+      number: 23,
+      occupy: function () {
+        alert(this.number);
+      }
+    };
+
+    var roomTwo = {
+      number: 23,
+      toJSON: function() {
+        return(this.number);
+      }
+    };
+
+    event = {
+      title: "Конференция",
+      date: new Date(Date.UTC(2014, 0, 1)),
+      room: room,
+      roomTwo: roomTwo
+    };
+
+    alert(JSON.stringify(event));
+    alert(JSON.stringify(room));
+    alert(JSON.stringify(roomTwo));
+  }
+
+  // Пример 4
+  // Исключение свойств из сериализации
+
+  function runDoc_4() {
+    var user = {
+      name: "Вася",
+      age: 25,
+      window: window
+    };
+
+    alert( JSON.stringify(user, ["name", "age"], 2) );
+
+    var str = JSON.stringify(user, function(key, value) {
+      if (key == 'window') return undefined;
+      return value;
+    });
+
+    alert( str );
+  }
+
+  // Задание 1
+
+  function runTask_1() {
+    var leader = {
+      name: "Иван Петров",
+      age: 34
+    };
+
+    var str = JSON.stringify(leader, "", 2);
+    alert(str);
+
+    var leaderFromStr = JSON.parse(str);
+    alert(leaderFromStr.name);
+  }
+
+  // Задание 2
+
+  function runTask_2() {
+    var leader = {
+      name: "Василий Иванович",
+      toJSON: function() {
+        return soldier
+      }
+    };
+
+    var soldier = {
+      name: "Петька",
+      toJSON: function() {
+        return this
+      }
+    };
+
+    // эти объекты ссылаются друг на друга!
+    leader.soldier = soldier;
+    soldier.leader = leader;
+
+    var team = [leader, soldier];
+
+    var teamStr = JSON.stringify(team);
+    alert(teamStr);
+  }
+
+  window.run = runTask_2;
 })();
