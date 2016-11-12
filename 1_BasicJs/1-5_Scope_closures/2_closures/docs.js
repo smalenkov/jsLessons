78fcd4b8c167e62973ffc4fe773051d8c5b74cc7
Vladimir Smalenkov <Vladimir Smalenkov>
2016-11-12 18:14:36 +0300

    add task 2 in closures

--- 1_BasicJs/1-5_Scope_closures/2_closures/docs.js
+++ 1_BasicJs/1-5_Scope_closures/2_closures/docs.js
@@ -8,22 +8,44 @@
 
       for (var i=0; i<arr.length; i++) {
 
-        var func = (function (arg) {
+        arrFunc.push((function (arg) {
           return function () {
             return arr[arg];
           }
-        })(i);
+        })(i));
 
-        arrFunc.push(func);
+        //arrFunc.push(func);
 
       }
       return arrFunc;
       // alert(arrFunc);
     }
 
-    //document.write(toArr([5,4,6,7]));
+    document.write(toArr([5,4,6,7]));
     document.write(toArr([5,4,6,7])[1]());
   }
 
-  window.run = runTask_1;
+  function runTask_2() {
+    function toArr(arr) {
+
+      var arrFunc = [];
+      var arg = [];
+
+      for (var i=0; i<arr.length; i++) {
+        arg = arr[i]; // так работать не будет
+        // у каждой функции в массиве должен быть свой контекст с переменной, хранящей значение элемента массива в замыкании
+        // у каждой вызываемой функции должно быть свое замыкание
+        arrFunc.push(function () {
+          return arg; // выведет только последний элемент из входящего массива
+        });
+
+      }
+      return arrFunc;
+    }
+
+    document.write(toArr([5,4,6,7]));
+    document.write(toArr([5,4,6,7])[1]());
+  }
+
+  window.run = runTask_2;
 })();
