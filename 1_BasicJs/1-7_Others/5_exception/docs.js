63e7573744a70f4465e3dc236f01f4fdd993af68
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-14 12:23:29 +0300

    add exception

---
+++ 1_BasicJs/1-7_Others/5_exception/docs.js
@@ -0,0 +1,10 @@
+;(function () {
+
+  // Пример 1
+
+  function runDoc_1() {
+    document.write('Сюда запилю код о перехвате ошибок, "try..catch". Но чуть позже :)');
+  }
+
+  window.run = runDoc_1;
+})();
