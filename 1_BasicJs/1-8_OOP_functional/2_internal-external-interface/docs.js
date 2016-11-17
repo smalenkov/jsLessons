266a7b3f9211d780b8d6783fb4f8db604d4e8eeb
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-17 17:05:25 +0300

    add private and public in oop

--- 1_BasicJs/1-8_OOP_functional/2_internal-external-interface/docs.js
+++ 1_BasicJs/1-8_OOP_functional/2_internal-external-interface/docs.js
@@ -4,10 +4,28 @@
 
   function runDoc_1() {
 
+    // mark, horsePower — приватные (локальные) свойства. Доступны только внутри
     function Auto(mark, horsePower) {
       this.mark = mark; // лошадиные силы
       this.horsePower = horsePower; // лошадиные силы
-      this.speed = 0; // лошадиные силы
+      this.speed = 0; // скорость
+
+      // приватные методы (вложенные функции)
+      // рассчет времени разгона
+      function getSpeedTime() {
+        return 2400;
+      }
+
+      // приватные методы (вложенные функции)
+      // по достижению нужной скорости
+      function onReady() {
+        alert('Ой-ой притормози, чувачок');
+      }
+
+      // go — публичный метод (доступен из вне)
+      this.go = function() {
+        setTimeout(onReady, getSpeedTime())
+      };
 
       document.write( 'Создана машина марки ' +  this.mark + '  мощностью: ' + this.horsePower + ' л.с.' );
     }
@@ -17,6 +35,8 @@
 
     auto.speed = 140;
     document.write(' Скорость авто ' + auto.speed);
+
+    auto.go();
 
   }
 
