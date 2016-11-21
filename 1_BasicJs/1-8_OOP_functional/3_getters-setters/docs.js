bde7ec0124e18f6f857834f10520b25d8ff66d93
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-21 11:19:27 +0300

    add getters setters in oop

--- 1_BasicJs/1-8_OOP_functional/3_getters-setters/docs.js
+++ 1_BasicJs/1-8_OOP_functional/3_getters-setters/docs.js
@@ -6,7 +6,7 @@
 
     // mark, horsePower — приватные (локальные) свойства. Доступны только внутри
     function Auto(mark, horsePower) {
-      this.speed = 0; // скорость
+      var speed = 0; // скорость (приватное свойство, устанавливается через setSpeed)
 
       // Ускорение авто
       var MAX_SPEEDUP = 6;
@@ -14,17 +14,32 @@
       // Максимальная скорость
       var MAX_SPEED = 240;
 
+      var thisObj = this;
       // приватные методы (вложенные функции)
       // рассчет времени разгона (формула выдуманная)
       var getSpeedTime = function() {
-        return this.speed / MAX_SPEEDUP * horsePower;
-      }.bind(this);
+        return speed / MAX_SPEEDUP * horsePower;
+      };
+
+      this.setSpeed = function (amount) {
+        if (amount > MAX_SPEED) {
+          throw new Error('Максимальная скорость авто ' + MAX_SPEED)
+        }
+        if (amount < 0) {
+          throw new Error('Скорость должна быть положительной')
+        }
+        speed = amount;
+      };
+
+      this.getSpeed = function () {
+        return speed;
+      };
 
       // приватные методы (вложенные функции)
       // по достижению нужной скорости
       var onReady = function onReady() {
-        alert('Ой-ой притормози, чувачок! Твоя скорость уже ' + this.speed);
-      }.bind(this);
+        alert('Ой-ой притормози, чувачок! Твоя скорость уже ' + speed);
+      };
 
       // таймер
       var timerId;
@@ -45,11 +60,12 @@
     // создать авто
     var auto = new Auto('BMW', 116);
 
-    auto.speed = prompt("Какая скорость?");
-    document.write(' Скорость авто ' + auto.speed);
+    var speed = prompt("Какая скорость?");
+    auto.setSpeed(speed);
+    document.write(' Скорость авто ' + auto.getSpeed());
 
     auto.go();
-    auto.stop();
+    //auto.stop();
 
   }
 
