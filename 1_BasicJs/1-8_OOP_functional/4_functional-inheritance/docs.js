1697c740724725f16be0f821ff8e3110fcb2f271
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-23 17:11:28 +0300

    add task 1

--- 1_BasicJs/1-8_OOP_functional/4_functional-inheritance/docs.js
+++ 1_BasicJs/1-8_OOP_functional/4_functional-inheritance/docs.js
@@ -37,7 +37,7 @@
 
       var parentEnable = this.enable;
 
-      this.enable = function () {
+      this.enable = function() {
         parentEnable();
         this.run();
       };
@@ -45,6 +45,7 @@
     }
 
     var coffeeMachine = new CoffeeMachine(10000);
+    coffeeMachine.run();
 
     coffeeMachine.enable();
     coffeeMachine.setWaterAmount(120);
@@ -54,5 +55,60 @@
 
   }
 
-  window.run = runDoc_1;
+
+  // Задание 1
+
+  function runTask_1() {
+
+    function Machine(power) {
+      this._power = power;
+      this._enabled = false; // Внутреннее свойство и перед ним ставится «_»
+
+      this.enable = function() {
+        this._enabled = true;
+      };
+
+      this.disable = function() {
+        this._enabled = false;
+      };
+    }
+
+    function CoffeeMachine(power) {
+      Machine.apply(this, arguments); // отнаследовать
+
+      var waterAmount = 0;
+
+      this.setWaterAmount = function(amount) {
+        waterAmount = amount;
+      };
+
+      this.getWaterAmount = function() {
+        return waterAmount;
+      };
+
+      function onReady() {
+        alert('Кофе готов!');
+      }
+
+      this.run = function () {
+        if (!this._enabled) {
+          throw new Error('Сначала включии ее :)');
+        }
+
+        setTimeout(onReady, 1000);
+      };
+
+
+    }
+
+    var coffeeMachine = new CoffeeMachine(10000);
+    //coffeeMachine.run();
+
+    var coffeeMachine2 = new CoffeeMachine(10000);
+    coffeeMachine2.enable();
+    coffeeMachine2.run(); // ...Кофе готов!
+
+  }
+
+  window.run = runTask_1;
 })();
