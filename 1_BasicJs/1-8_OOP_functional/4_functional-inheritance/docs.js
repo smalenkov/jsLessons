4c0203217a2d99600d0520c8fd50697a9bf39b45
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-11-23 11:28:37 +0300

    add exemple one

--- 1_BasicJs/1-8_OOP_functional/4_functional-inheritance/docs.js
+++ 1_BasicJs/1-8_OOP_functional/4_functional-inheritance/docs.js
@@ -3,7 +3,42 @@
   // Пример 1
 
   function runDoc_1() {
-    document.write('<h1>Hello</h1>');
+
+    function Machine() {
+      var enabled = false;
+
+      this.enable = function() {
+        enabled = true;
+      };
+
+      this.disable = function() {
+        enabled = false;
+      };
+    }
+
+    function CoffeeMachine(power) {
+      Machine.call(this); // отнаследовать
+
+      var waterAmount = 0;
+
+      this.setWaterAmount = function(amount) {
+        waterAmount = amount;
+      };
+
+      this.getWaterAmount = function() {
+        return waterAmount;
+      }
+
+    }
+
+    var coffeeMachine = new CoffeeMachine(10000);
+
+    coffeeMachine.enable();
+    coffeeMachine.setWaterAmount(120);
+    coffeeMachine.disable();
+
+    document.write(coffeeMachine.getWaterAmount());
+
   }
 
   window.run = runDoc_1;
