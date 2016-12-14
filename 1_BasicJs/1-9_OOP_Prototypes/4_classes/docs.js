df17743f3cc4820aa1905bf1a54e896d329c1d00
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-12-14 13:28:26 +0300

    prototype class

--- 1_BasicJs/1-9_OOP_Prototypes/4_classes/docs.js
+++ 1_BasicJs/1-9_OOP_Prototypes/4_classes/docs.js
@@ -5,19 +5,19 @@
   function runDoc_1() {
 
     function Animal(name) {
-      this.name = name,
+      this._name = name; // _name приватное (защищенное) свойство
       this.speed = 0
     }
 
     Animal.prototype.run = function(speed) {
       this.speed += speed;
-      document.write(this.name + ' бежит со скоростью ' + this.speed + '<br>');
-    }
+      document.write(this._name + ' бежит со скоростью ' + this.speed + '<br>');
+    };
 
     Animal.prototype.stop = function() {
       this.speed = 0;
-      document.write(this.name + ' остановился и его скорость = ' + this.speed + '<br>');
-    }
+      document.write(this._name + ' остановился и его скорость = ' + this.speed + '<br>');
+    };
 
     var beast = new Animal('Животное');
 
@@ -28,5 +28,37 @@
 
   }
 
-  window.run = runDoc_1;
+
+  // Задание 1
+
+  function runTask_1() {
+
+    function CoffeeMachine(power) {
+      this._power = power;
+      this.waterAmount = 0;
+
+      this.WATER_HEAT_CAPACITY = 4200;
+
+      this.getTimeToBoil = function() {
+        return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this._power;
+      };
+    }
+
+    CoffeeMachine.prototype.run = function() {
+      setTimeout(function() {
+        alert('Кофе готов!');
+      }, this.getTimeToBoil());
+    };
+
+    CoffeeMachine.prototype.setWaterAmount = function(amount) {
+      this.waterAmount = amount;
+    };
+
+    var coffeeMachine = new CoffeeMachine(10000);
+    coffeeMachine.setWaterAmount(50);
+    coffeeMachine.run();
+
+  }
+
+  window.run = runTask_1;
 })();
