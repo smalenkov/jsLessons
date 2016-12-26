a9fb33fb6ce082790a8b50cd1c319e31d2b628c9
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-12-26 17:16:42 +0300

    add events parts

---
+++ 2_UI/2-1_document/2_dom-nodes/docs.js
@@ -0,0 +1,12 @@
+;(function () {
+
+  // Пример 1
+
+  function runTask_1() {
+
+    alert(document.body);
+
+  }
+
+  window.run = runTask_1;
+})();
---
+++ 2_UI/2-2_events-and-interfaces/1_introduction-browser-events/docs.js
@@ -0,0 +1,14 @@
+;(function () {
+
+  // Пример 1
+
+  function runDoc_1() {
+
+    for (var i = 1; i <= 3; i++) {
+      alert("Кролик номер " + i);
+    }
+
+  }
+
+  window.run = runDoc_1;
+})();
