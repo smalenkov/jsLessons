4f2925fc51ce1c50f4b5648437afaa0a924aea7a
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-12-28 17:31:09 +0300

    add sort variants

--- MyPoligon/SortVariants/docs.js
+++ MyPoligon/SortVariants/docs.js
@@ -1,13 +1,26 @@
 ;(function () {
 
+  var arr = [3, 1, 5, 9, 12, 55, 100, 23, 13, 97, 62, 31];
 
   // Сортировка пузырьком
 
-  function runSort() {
+  function runSortBubble() {
 
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = arr.length - 1; j > i; j--) {
+        if (arr[j - 1] > arr[j]) {
+          var x = arr[j - 1];
+          arr[j - 1] = arr[j];
+          arr[j] = x;
+        }
+      }
+    }
 
+    document.write(arr);
+    document.write("<br> Длинна массива: " + arr.length);
 
   }
 
-  window.run = runTest_2;
+
+  window.run = runSortBubble;
 })();
