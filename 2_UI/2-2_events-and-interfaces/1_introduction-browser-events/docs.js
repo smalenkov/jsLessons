a54dc39bb12d165b7f0152ca2749c77e79f69419
Vladimir Smalenkov <smalenkoff@gmail.com>
2016-12-28 13:56:24 +0300

    add task

--- 2_UI/2-2_events-and-interfaces/1_introduction-browser-events/docs.js
+++ 2_UI/2-2_events-and-interfaces/1_introduction-browser-events/docs.js
@@ -20,7 +20,7 @@
     };
 
     function sayHi() {
-      this.value = 'Запустилось';
+      this.value = 'Запустилось'; // get event element from this
       alert('Hi!');
     }
 
@@ -34,8 +34,20 @@
   function runDoc_3() {
 
     var elem = document.getElementById('rabbits-button');
-    elem.addEventListener("click", sayHi);
-    elem.addEventListener("mouseover", sayThinks);
+    elem.addEventListener('click', sayHi);
+    elem.addEventListener('mouseover', sayThinks);
+
+    var recbox = document.getElementById('recbox');
+    recbox.addEventListener('click', widthAnimate);
+    recbox.addEventListener('transitionend', setColor);
+
+    function widthAnimate() {
+      this.classList.add('widthplus');
+    }
+
+    function setColor() {
+      this.classList.add('newcolor');
+    }
 
     function sayHi() {
       alert('Hiiii!');
@@ -48,5 +60,48 @@
 
   }
 
-  window.run = runDoc_3;
+  // Задание 1
+
+  function runTask_1() {
+
+    var tButton = document.getElementById('hidetextbutton');
+    tButton.addEventListener('click', hideText);
+    
+    var hButton = document.getElementById('hidebutton');
+    hButton.addEventListener('click', hideButton);
+
+    var menuTitle = document.getElementById('menu-title');
+    menuTitle.addEventListener('click', toggleMenu);
+
+    var closeButton = document.getElementsByClassName('close-button');
+    for (var i=0; i<closeButton.length; i++) {
+      closeButton[i].addEventListener('click', closeMessage);
+    }
+
+    function hideText() {
+      var text = document.getElementById('text');
+      text.classList.add('hide');
+    }
+
+    function hideButton() {
+      this.classList.add('hide');
+    }
+
+    function toggleMenu() {
+      var menu = document.getElementById('menu');
+      if(!menu.classList.contains('hide')) {
+        menu.classList.add('hide')
+      } else {
+        menu.classList.remove('hide')
+      }
+    }
+
+    function closeMessage() {
+      var animalBox = this.parentNode;
+      animalBox.classList.add('hide');
+    }
+
+  }
+
+  window.run = runTask_1;
 })();
