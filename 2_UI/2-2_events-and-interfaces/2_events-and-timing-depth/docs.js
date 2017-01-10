284d2c3a3d74771977d3b73efeac5bc940176f23
Vladimir Smalenkov <smalenkoff@gmail.com>
2017-01-10 14:57:29 +0300

    add events for input

--- 2_UI/2-2_events-and-interfaces/2_events-and-timing-depth/docs.js
+++ 2_UI/2-2_events-and-interfaces/2_events-and-timing-depth/docs.js
@@ -12,6 +12,9 @@
     var inputFormFathername = document.getElementById('input-fathername');
     var inputFormName = document.getElementById('input-name');
 
+    var button = document.getElementById('input-button');
+    var inputText = document.getElementById('input-text');
+
     function sayClick() {
       pastText.innerHTML += "Click "
     }
@@ -38,6 +41,21 @@
       };
     }
 
+    function clickButton() {
+      inputText.value += 'начало-';
+
+      // При отсутствии setTimeout событе сработает сразу же (синхронно)
+      setTimeout(function() {
+        inputText.focus();
+      }, 0);
+
+      inputText.value += '-конец  ';
+    }
+
+    inputText.onfocus = function() {
+      inputText.value += ' !focus! ';
+    };
+
     elem.addEventListener('click', sayClick);
     elem.addEventListener('mousedown', sayMouseDown);
     elem.addEventListener('mouseup', sayMouseUp);
@@ -48,6 +66,9 @@
     inputFormName.addEventListener('focus', setFocusValue('hello Name'));
     inputFormLastname.addEventListener('focus', setFocusValue('hello Lastname'));
     inputFormFathername.addEventListener('focus', setFocusValue('hello Fathername'));
+
+    //inputText.addEventListener('focus', setTextFocusValue('Focus'));
+    button.addEventListener('click', clickButton);
 
   }
 
