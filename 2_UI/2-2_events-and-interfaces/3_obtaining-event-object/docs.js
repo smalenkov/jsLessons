22dd3799e67569569cd41507edd748db4c4dc081
Vladimir Smalenkov <smalenkoff@gmail.com>
2017-01-10 17:38:59 +0300

    add task_1

--- 2_UI/2-2_events-and-interfaces/3_obtaining-event-object/docs.js
+++ 2_UI/2-2_events-and-interfaces/3_obtaining-event-object/docs.js
@@ -4,9 +4,32 @@
 
   function runDoc_1() {
 
-    alert('hello');
+    var inputButton = document.getElementById('input-button');
+
+    function startEvent(e) {
+      alert(e.type);
+      e.currentTarget.value = 'Дави еще!)';
+    }
+
+    inputButton.addEventListener('click', startEvent)
 
   }
 
-  window.run = runDoc_1;
+
+  // Задачка 1
+
+  function runTask_1() {
+
+    var ball = document.getElementById('ball');
+    var garden = document.getElementById('garden');
+
+    function moveBall(e) {
+      ball.style.top = e.clientY + 'px';
+    }
+
+    garden.addEventListener('click', moveBall);
+
+  }
+
+  window.run = runTask_1;
 })();
