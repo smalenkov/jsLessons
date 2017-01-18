966c9b95bb25104a2f9d67fee664da3f30d033a0
Vladimir Smalenkov <smalenkoff@gmail.com>
2017-01-18 17:03:21 +0300

    with cors

--- MyPoligon/handlebarsjsTest/server.js
+++ MyPoligon/handlebarsjsTest/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path'); // модуль для парсинга пути
+var cors = require('cors');
 var log = require('./libs/log')(module);
 var PostsModel = require('./libs/mongoose').PostsModel;
 var app = express();
@@ -8,11 +9,6 @@ app.listen(1337, function(){
   console.log('Express server listening on port 1337');
 });
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 app.get('/', function(req, res) {
   res.send('Star express!');
@@ -26,11 +22,11 @@ app.get('/api', function (req, res) {
   res.send('API is running');
 });
 
-app.get('/api/posts', function(req, res) {
+app.get('/api/posts', cors(), function(req, res) {
   return PostsModel.find(function (err, posts) {
     if (!err) {
       res.statusCode = 200;
-      return res.send(posts[0]);
+      return res.json(posts[0]);
     } else {
       res.statusCode = 500;
       log.error('Internal error(%d): %s',res.statusCode,err.message);
