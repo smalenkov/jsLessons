ca749da2f9e40ef1c958421b027e93158ce2c09b
Vladimir Smalenkov <smalenkoff@gmail.com>
2017-12-09 20:07:06 +0300

    new scripts for translate app

--- MyPoligon/translate-memorize/js/translatememo.js
+++ MyPoligon/translate-memorize/js/translatememo.js
@@ -1,6 +1,16 @@
 let btn = document.getElementById('translate-btn');
 btn.addEventListener('click', runRequest);
 
+function success(data) {
+  let listEl = document.createElement('li');
+  listEl.innerText = data.text[0];
+
+  let contentEl = document.getElementById('translate-list');
+
+  contentEl.appendChild(listEl);
+  contentEl.insertBefore(listEl, contentEl.firstChild);
+}
+
 let options = {
   method: "POST",
   mode: 'cors',
@@ -10,17 +20,15 @@ let options = {
 };
 
 function runRequest() {
-  options['body'] = 'text=' + document.getElementById('totranslate').value;
+  let inputText = document.getElementById('totranslate').value;
+  options['body'] = `text=${inputText}`;
 
   fetch('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20171209T032315Z.9f84871bb747909b.328bbdae561f9d2a8ced6b765482c89c53e42d7e&lang=en-ru', options)
     .then(function(response) {
-      return response.json();
+      if (response.status === 200) {
+        return response.json();
+      }
     })
-    .then(function(data) {
-      let contentEl = document.getElementById('translate');
-      contentEl.innerText = data.text[0];
-    })
-    .catch(console.log('Request error!'));
+    .then(success)
+    .catch( () => {throw new Error('Request from translate API error...')} );
 }
-
-// let text = document.getElementById('totranslate').textContent;
